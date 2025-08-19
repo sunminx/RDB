5bad3b8f658513c7e2a33bc73c0034ff4e036981
sunminx <2421500571@qq.com>
2025-08-19 17:37:19 +0800

    add: randomKV test

--- internal/datastruct/dict_test.go
+++ internal/datastruct/dict_test.go
@@ -1,1 +1,21 @@
 package datastruct
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestRandomKV(t *testing.T) {
+	d := NewDict()
+	cnt := 10000
+	for i := 0; i < cnt; i++ {
+		d.Put(strconv.Itoa(i), i)
+	}
+
+	for i := 0; i < cnt; i++ {
+		_, _, ok := d.RandomKV()
+		if !ok {
+			t.Error("failed to get random kv")
+		}
+	}
+}
